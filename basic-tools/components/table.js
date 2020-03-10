9ef49a24782f25d4dce559e2c076be9a2c01929d
Ala'a <aalhabashna@gmail.com>
2020-03-10 10:18:24 -0400

    Added basic table

--- basic-tools/components/table.js
+++ basic-tools/components/table.js
@@ -10,8 +10,42 @@ export default Core.Templatable("Basic.Components.Table", class Table extends Te
 		super(container, options);
 	}
 	
-	Template() {        
-		return "<table handle='root' class='table'>" +
-			   "</table>";
+	Template() {
+
+		let a = [1, 2, 3, 4, 5];
+
+		//return "<table id= 'myTable'> <tr> <th>Name</th> <th>Favorite Color</th> </tr> <tr> <td>Bob</td> <td>Yellow</td> </tr> <tr> <td>Michelle</td> <td>Purple</td> </tr> </table>"
+
+		//return "<div> <table id ='myTable'> <thead> <tr> <th>Id</th><th>Name</th><th>Mark</th> </tr> </thead> </table> </div>"
+    return "<div> <table id ='myTable'>   </table> </div>"
 	}
+
+
+  addOneRow(table){
+
+  	 	const FIELDS = ["id", "name", "mark"];
+  	 	const values  = ["aaa","aaa","aaa"];
+
+
+       let tableRef = document.getElementById('myTable');
+       let row = tableRef.insertRow();
+       for (const value of values) {
+       	row.insertCell().textContent = value;
+              
+      }
+  }
+
+
+    addOneRow2(table){
+    	let tableRef = document.getElementById('myTable');
+      // Insert a row at the end of the table
+      let newRow = tableRef.insertRow(-1);
+      // Insert a cell in the row at index 0
+      let newCell = newRow.insertCell(0);
+      // Append a text node to the cell
+      let newText = document.createTextNode('New bottom row');
+      newCell.appendChild(newText);
+    }
+
+
 })
