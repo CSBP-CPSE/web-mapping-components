fd914d01be6aa852107eae7ed40a9c35ed38830c
Robert Oikle <robert.oikle@canada.ca>
2020-11-17 16:43:18 -0500

    Break out Choropleth method logic
    - Added function to get style details from legend item
    - Added function to get a list of styles from legend
    - Added function to generate style classes based on legend styles
    -  Replaced Choropleth logic with added functions.

--- mapbox-tools/components/map.js
+++ mapbox-tools/components/map.js
@@ -151,28 +151,96 @@ export default class Map extends Evented {
 		}
 	}
 
+	/**
+	 * Gets the style data from provided legend item
+	 * @param {object} legendItem - Object containing the style information 
+	 * @retruns - An object containing the legendItem color and value if available
+	 */
+	GetStylingFromLegendItem(legendItem) {
+		let style = {};
+
+		if (legendItem.color) {
+			style.color = legendItem.color;
+
+			if (legendItem.value) {
+				style.value = legendItem.value;
+			}
+		}
+
+		return style;
+	}
+
+	/**
+	 * Gets a list of style colours and values defined in the map config legend.
+	 * @param {array} legendItems - List of legend items containing style rules
+	 * @returns - A list of style objects containing colours and conditions
+	 * needed to paint layers with that colour.
+	 */
+	GetListOfStyles(legendItems) {
+		let i, legendItem;
+		let styleCollection = [];
+
+		// Iterate through legendItems and get styling from each
+		if (Array.isArray(legendItems)) {
+			for (i = 0; i < legendItems.length; i += 1) {
+				legendItem = legendItems[i];
+				styleCollection.push(this.GetStylingFromLegendItem(legendItem));
+			}
+		}
+
+		return styleCollection;
+	}	
+
+	/**
+	 * Generate all style classes defined in the map config file.
+	 * @param {object} styles - object containing the legend details stored in
+	 * the map config file.
+	 * @param {array} opacities - list of opacity values
+	 * @retruns - A list of colour classes using style data and opacity values.
+	 */
+	GenerateColourClasses(styles, opacities) {
+		var updatedColor, i, styleItem, defaultColour;
+		var styleOpacities = opacities || 1;
+		var classes = ['case'];
+		var legendStyles = this.GetListOfStyles(styles);
+		
+		// Check that legend items length equals opacity length
+		if (legendStyles.length) {
+			for (i = 0; i < legendStyles.length; i += 1) {
+				styleItem = legendStyles[i];
+
+				// Get color for style 
+				if (Array.isArray(styleOpacities) && styleOpacities.length) {
+					updatedColor = styleItem.color.length == 3 ? `rgba(${styleItem.color.join(',')},${styleOpacities[i]})` : `rgba(${styleItem.color.join(',')})`;
+				} else {
+					updatedColor = styleItem.color.length == 3 ? `rgba(${styleItem.color.join(',')},${styleOpacities})` : `rgba(${styleItem.color.join(',')})`;
+				}
+
+				if (styleItem.value && updatedColor) {
+					// Add mapbox expression value is defined, add it to classes list
+					classes.push(styleItem.value);						
+
+					// Add colour to classes list
+					classes.push(updatedColor);
+				} else {
+					defaultColour = updatedColor;
+				}
+			}
+
+			// Add default colour as last item in colour classes
+			// This is required by mapbox to in sure errors 
+			// don't occur when the last item in the legend config
+			// is not the default colour (i.e. the one without a 
+			// a defined mapbox expression value)
+			classes.push(defaultColour);
+		}
+
+		return classes;
+	}
+
 	/*This is used with an array of colors and (single opacity or array of opacity values)*/
 	Choropleth(layers, property, legend, opacity) {
-		var classes = ['case'];
-
-		if(Array.isArray(opacity) && Array.isArray(legend) && legend.length > 1){
-			legend.forEach(function(l, index) {			
-			var color = l.color.length == 3 ? `rgba(${l.color.join(',')},${opacity[index]})` : `rgba(${l.color.join(',')})`;
-			
-			if (l.value) classes.push(l.value);
-			
-			classes.push(color);
-		});
-		}
-		else if(!Array.isArray(opacity) &&  Array.isArray(legend) && legend.length > 1) {
-			legend.forEach(function(l) {			
-			var color = l.color.length == 3 ? `rgba(${l.color.join(',')},${opacity})` : `rgba(${l.color.join(',')})`;
-			
-			if (l.value) classes.push(l.value);
-			
-			classes.push(color);
-		});
-		}
+		var classes = this.GenerateColourClasses(legend, opacity);
 		layers.forEach(l => {
 			this.original[l] = this.map.getPaintProperty(l, property);
 			this.SetPaintProperty(l, property, classes);
@@ -181,22 +249,38 @@ export default class Map extends Evented {
 
 	/*This is used with a single color value and an array of opacity values)*/
 	ChoroplethVarOpac(layers, property, legend, opacity) {
-		var col = [0,0,0];
-		var classes = ['case'];
+		let color, i, defaultColour, updatedColor;
+		var classes = this.GenerateColourClasses(legend, opacity);
 
+		// Over-ride classes if the property is 'text-halo-color, text-color,
+		// or a circle-stroke-color
 		if (property === 'text-halo-color') {
-			col = [255,255,255];
+			color = [255,255,255];
+		} else if (property === 'text-color' || property === 'circle-stroke-color') {
+			color = [0,0,0];
 		}
 
-		if(Array.isArray(opacity) && Array.isArray(legend) && legend.length > 1){
+		// If an over-ride colour exists, than regenerate style classes
+		if (color) {
+			classes = ['case'];
+
 			legend.forEach(function(l, index) {			
 			
-			var color = `rgba(${col.join(',')},${opacity[index]})`;
+				updatedColor = `rgba(${color.join(',')},${opacity[index]})`;
 			
-			if (l.value) classes.push(l.value);
-			
-			classes.push(color);
-		});
+				if (l.value) {
+					classes.push(l.value);
+		
+					classes.push(updatedColor);
+				} else {
+					defaultColour = updatedColor;
+				}
+
+			});
+
+			// Make sure catch-all/default color is at end of mapbox expression
+			// represnting the colour classes.
+			classes.push(defaultColour);
 		}
 
 		layers.forEach(l => {
