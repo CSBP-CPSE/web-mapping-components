1645a78e32fe5c702d361020f07c5f61839c7e10
Robert Oikle <robert.oikle@canada.ca>
2021-02-04 14:36:29 -0500

    Added banner text to rollup config script

--- config/web-mapping-components.rollup.js
+++ config/web-mapping-components.rollup.js
@@ -1,11 +1,20 @@
 import babel from 'rollup-plugin-babel';
 
+const banner = 
+`/*
+ * Web Mapping Components
+ * https://github.com/CSBP-CPSE/web-mapping-components/blob/master/LICENCE.md
+ * v1.0 - 2021-02-04
+ */
+`;
+
 export default {
     input: '../web-mapping-components/src/main.js',
     output: [
 		{
 	        file: '../web-mapping-components/dist/web-mapping-components.min.js',
-	        format: 'es'
+			format: 'es',
+			banner: banner
 		}
 	],
     plugins: [
