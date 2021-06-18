bf30a87a20424289f147b67c341d4fd08adc9132
Robert Oikle <robert.oikle@canada.ca>
2021-06-18 12:09:41 -0400

    Extended search control
    - Added documentation
    - Added getter/setters
    - Added method for updating control list

--- src/mapbox-tools/controls/search.js
+++ src/mapbox-tools/controls/search.js
@@ -14,6 +14,7 @@ export default class Search extends Control {
 		this.layer = options.layer;
 		this.field = options.field;
 		this.color = options.color;
+		this.items = options.items;
 		
 		// TODO : This should probably happen outside of the widget.
 		this.Node('typeahead').items = this.Itemize(options.items);
@@ -22,11 +23,59 @@ export default class Search extends Control {
 	
 		this.Node('typeahead').On('Change', this.onTypeaheadChange_Handler.bind(this));
 	}
+
+	/**
+	 * Set the search layer for the search control
+	 * @param {string} layer Id of the search layer
+	 */
+	set searchLayer(layer) {
+		this.layer = layer
+	}
+
+	/**
+	 * Get the search layer used by the search control
+	 * @returns {string} Id of the current search layer
+	 */
+	get searchLayer() {
+		return this.layer;
+	}
+
+	/**
+	 * Set the search items associated with the current search control
+	 * @param {array} items List of search items
+	 */
+	set searchItems(items) {
+		this.items = items;
+	}
+
+	/**
+	 * Get search items associated with the current search control
+	 * @returns {array} List of search items
+	 */
+	get searchItems() {
+		return this.items;
+	}
 	
+	/**
+	 * Update the available items for the typeahead element
+	 */
+	UpdateSearchItems() {
+		this.Node('typeahead').items = this.Itemize(this.items);
+	}
+
+	/**
+	 * Itemize the search items for the search control
+	 * @param {array} items 
+	 * @returns {array} list of sorted items
+	 */
 	Itemize(items) {		
 		return items.sort((a, b) => { return a.label > b.label ? 1 : -1 });
 	}
 	
+	/**
+	 * Event handler for typeahead changes
+	 * @param {object} ev typeahead change event
+	 */
 	onTypeaheadChange_Handler(ev) {
 		var data = {
 			layer : this.layer,
@@ -38,6 +87,10 @@ export default class Search extends Control {
 		this.Emit('Change', data);
 	}
 	
+	/**
+	 * Create a template for the search control
+	 * @returns {string} html template for search control
+	 */
 	Template() {        
 		return "<div handle='root' class='search-control mapboxgl-ctrl'>" +
 				  "<div handle='typeahead' widget='Basic.Components.Typeahead'></div>" +
