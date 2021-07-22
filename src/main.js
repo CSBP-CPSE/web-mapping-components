62fd2f5275351d21f447991a89e8c7428115554d
Robert Oikle <robert.oikle@canada.ca>
2021-07-22 13:11:42 -0400

    Added Table module export to main.js

--- src/main.js
+++ src/main.js
@@ -5,27 +5,28 @@ export { default as Dom } from './basic-tools/tools/dom.js';
 export { default as Net } from './basic-tools/tools/net.js';
 export { default as Store } from './basic-tools/tools/store.js';
 
-export { default as Evented } from './basic-tools/components/evented.js'
-export { default as Templated } from './basic-tools/components/templated.js'
-export { default as Popup } from './basic-tools/components/popup.js'
-export { default as Tooltip } from './basic-tools/components/tooltip.js'
-export { default as Typeahead } from './basic-tools/components/typeahead.js'
+export { default as Evented } from './basic-tools/components/evented.js';
+export { default as Templated } from './basic-tools/components/templated.js';
+export { default as Popup } from './basic-tools/components/popup.js';
+export { default as Table } from './basic-tools/components/table.js';
+export { default as Tooltip } from './basic-tools/components/tooltip.js';
+export { default as Typeahead } from './basic-tools/components/typeahead.js';
 
 // Aggregation of mapbox-tools modules 
-export { default as Bookmarks } from './mapbox-tools/controls/bookmarks.js'
-export { default as Download } from './mapbox-tools/controls/download.js'
-export { default as Fullscreen } from './mapbox-tools/controls/fullscreen.js'
-export { default as Group } from './mapbox-tools/controls/group.js'
-export { default as Legend } from './mapbox-tools/controls/legend.js'
-export { default as MapsList } from './mapbox-tools/controls/mapsList.js'
-export { default as Menu } from './mapbox-tools/controls/menu.js'
-export { default as Navigation } from './mapbox-tools/controls/navigation.js'
-export { default as Opacity } from './mapbox-tools/controls/opacity.js'
-export { default as Search } from './mapbox-tools/controls/search.js'
-export { default as Toc } from './mapbox-tools/controls/toc.js'
+export { default as Bookmarks } from './mapbox-tools/controls/bookmarks.js';
+export { default as Download } from './mapbox-tools/controls/download.js';
+export { default as Fullscreen } from './mapbox-tools/controls/fullscreen.js';
+export { default as Group } from './mapbox-tools/controls/group.js';
+export { default as Legend } from './mapbox-tools/controls/legend.js';
+export { default as MapsList } from './mapbox-tools/controls/mapsList.js';
+export { default as Menu } from './mapbox-tools/controls/menu.js';
+export { default as Navigation } from './mapbox-tools/controls/navigation.js';
+export { default as Opacity } from './mapbox-tools/controls/opacity.js';
+export { default as Search } from './mapbox-tools/controls/search.js';
+export { default as Toc } from './mapbox-tools/controls/toc.js';
 
-export { default as Factory } from './mapbox-tools/tools/factory.js'
-export { default as Other } from './mapbox-tools/tools/other.js'
+export { default as Factory } from './mapbox-tools/tools/factory.js';
+export { default as Other } from './mapbox-tools/tools/other.js';
 
-export { default as Control } from './mapbox-tools/components/control.js'
-export { default as Map } from './mapbox-tools/components/map.js'
+export { default as Control } from './mapbox-tools/components/control.js';
+export { default as Map } from './mapbox-tools/components/map.js';
