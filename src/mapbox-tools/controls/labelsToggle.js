6385ceed87d21ac67c6f1d2e9e8bdc83b80faf3a
Robert Oikle <robert.oikle@canada.ca>
2021-07-29 15:18:14 -0400

    Initial commit for Labels Toggle Control

---
+++ src/mapbox-tools/controls/labelsToggle.js
@@ -0,0 +1,66 @@
+import Control from '../components/control.js';
+import Core from '../../basic-tools/tools/core.js';
+import Dom from '../../basic-tools/tools/dom.js';
+
+/**
+ * LabelsToggle class
+ * @class
+ */
+export default class LabelsToggle extends Control { 
+		
+	constructor(options) {	
+		super(options);
+		
+		this.map = options.map;
+
+		this._container = this.Node('root');
+
+		// If a custom label is provided, update menu label
+		if (options.label && typeof(options.label) === 'string') {
+			this.Node('labels-toggle-label').innerHTML = options.label;			
+		}
+
+		// Add event listeners for change events on maps menu
+		this.Node('labels-toggle-checkbox').addEventListener('change', this.onLabelsToggleCheckboxChange_Handler.bind(this));
+	}
+
+	getLabelLayers() {
+		let layerIds = [];
+
+		return layerIds;
+	}
+
+	/**
+	 * Handle labels toggle checkbox changes
+	 * @param {object} ev Change event
+	 */
+	onLabelsToggleCheckboxChange_Handler(ev) {
+		console.log(ev);
+		let layerIds = this.getLabelLayers();
+
+		if (ev) {
+			for (let i = 0; i < layerIds.length; i += 1) {
+				let layerId = layerIds[i];
+				this.map.HideLayer(layerId);
+			}
+		} else {
+			for (let i = 0; i < layerIds.length; i += 1) {
+				let layerId = layerIds[i];
+				this.map.ShowLayer(layerId);
+			}
+		}
+	}
+
+	/**
+	 * HTML Template for Labels Toggle Control
+	 * @returns {string} Template representing a labels toggle control
+	 */
+	 Template() {
+		return "<div handle='root' class='labels-toggle mapboxgl-ctrl'>" + 
+					"<div class='labels-toggle-container'>" + 
+						"<input type='checkbox' aria-label='Labels' handle='labels-toggle-checkbox' name='labels-toggle-checkbox' class='labels-toggle-checkbox'></input>" +
+						"<label handle='labels-toggle-label' class='labels-toggle-label'>Labels</label>" +
+					"</div>" +
+			   "</div>"
+	}
+}
